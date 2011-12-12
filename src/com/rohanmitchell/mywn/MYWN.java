be5c26d35d630b4f05e40a6ac89eb66c6fe91251
Rohan Mitchell <rohan@rohanmitchell.com>
2011-12-12 18:19:25 +1100

    Correct screen layout, add stub list items

--- src/com/rohanmitchell/mywn/MYWN.java
+++ src/com/rohanmitchell/mywn/MYWN.java
@@ -1,15 +1,17 @@
 package com.rohanmitchell.mywn;
 
-import android.app.Activity;
+import android.app.ListActivity;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
 
-public class MYWN extends Activity
+public class MYWN extends ListActivity
 {
-    /** Called when the activity is first created. */
     @Override
     public void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
+
+	setListAdapter(new ArrayAdapter<String>(this, R.layout.task_row, new String[] {"One", "Two", "Three"}));
     }
 }
